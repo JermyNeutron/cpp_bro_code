1f279de449c9116e125902ea2aa48fcb54dd5c28
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-06-16 15:12:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# "C++ Full Course for free" by Bro Code
+
+All of my work learning C++ by Bro Code's YouTube video.
---
+++ basics/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+std means "standard"
+cout means "character output"
+<< means "output"
+for int functions, return 0 for clear, 1 for error
+*/
+
+int main()
+{
+    // this is a comment
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << '\n';
+    return 0;
+}
