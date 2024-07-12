395819f0245bd9982264eb1a5803eeb8148fd1a4
Jeremy Desktop <jeremycrisostomo@gmail.com>
2024-07-12 02:25:29 -0700

    practicing character outputs and inputs

--- basics/helloworld.cpp
+++ basics/helloworld.cpp
@@ -7,10 +7,17 @@ cout means "character output"
 for int functions, return 0 for clear, 1 for error
 */
 
+// int main()
+// {
+//     // this is a comment
+//     std::cout << "I like pizza!" << '\n';
+//     std::cout << "It's really good!" << '\n';
+//     return 0;
+// }
+
 int main()
 {
     // this is a comment
-    std::cout << "I like pizza!" << '\n';
-    std::cout << "It's really good!" << '\n';
+    std::cout << "Hello world!" << "\n";
     return 0;
 }
